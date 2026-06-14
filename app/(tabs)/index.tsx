2c10f74c39e02e003f1841e8347e2ec7e33a3eb0
Topo600 <r.malina@protonmail.com>
2026-06-14 20:09:05 +0200

    Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+//dont use app here as name
+export default function homescreen() {
+  return (
+      <View>
+        <Text>hello surfers !</Text>
+      </View>
+  );
+}
